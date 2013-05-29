877582aed7a65672fdedaebe54476221831439d6
Björn Ritzl <bjorn.ritzl@gmail.com>
2013-05-29 11:29:49 +0200

    linkification of string to spannable string

--- src/se/springworks/android/utils/view/LinkifyUtil.java
+++ src/se/springworks/android/utils/view/LinkifyUtil.java
@@ -1,5 +1,6 @@
 package se.springworks.android.utils.view;
 
+import android.text.SpannableString;
 import android.text.util.Linkify;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,5 +32,15 @@ public class LinkifyUtil {
 		if(v instanceof ViewGroup) {
 			linkifyAllTextViews((ViewGroup)v, mask);
 		}
+	}
+	
+	public static SpannableString linkifyAll(String text) {
+		return linkify(text, Linkify.ALL);
+	}
+	
+	public static SpannableString linkify(String text, int mask) {
+		SpannableString s = new SpannableString(text);
+		Linkify.addLinks(s, mask);
+		return s;
 	}
 }
