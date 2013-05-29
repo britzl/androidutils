248343ef363540aa679ffe6f3d8f9f182322abcd
Björn Ritzl <bjorn.ritzl@gmail.com>
2013-05-29 11:29:22 +0200

    several improvements to the dialog builder, linkification of the message and so on

--- src/se/springworks/android/utils/dialog/DialogBuilder.java
+++ src/se/springworks/android/utils/dialog/DialogBuilder.java
@@ -1,52 +1,126 @@
 package se.springworks.android.utils.dialog;
 
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 public class DialogBuilder {
+	
+	private Builder builder;
+	
+	private Context context;
+	
+	public DialogBuilder(Context context) {
+		this.context = context;
+	}
+	
+	public static DialogBuilder create(Context context, int titleId, int messageId) {
+		return create(context, context.getString(titleId), context.getString(messageId));
+	}
+	
+	public static DialogBuilder create(Context context, int titleId, SpannableString message) {
+		return create(context, context.getString(titleId), message);
+	}
+	
+	public static DialogBuilder create(Context context, String title, String message) {
+		return create(context, title, new SpannableString(message));
+	}
+	
+	public static DialogBuilder create(Context context, String title, SpannableString message) {
+		DialogBuilder b = new DialogBuilder(context);
+		b.builder = new AlertDialog.Builder(context);
+		b.builder.setTitle(title);
+		b.builder.setMessage(message);
+		
+//		TextView tv = new TextView(context);
+//		tv.setText(message);
+//		tv.setAutoLinkMask(Linkify.ALL);
+//		tv.setMovementMethod(LinkMovementMethod.getInstance());
+//		
+//		b.builder.setView(tv);
+		return b;
+	}
+	
+	public DialogBuilder setCancelable(boolean cancelable) {
+		builder.setCancelable(cancelable);
+		return this;
+	}
+	
+	public DialogBuilder addNegativeButton(int cancelId) {
+		return addNegativeButton(context.getString(cancelId), null);
+	}
+	public DialogBuilder addNegativeButton(int cancelId, DialogInterface.OnClickListener listener) {
+		return addNegativeButton(context.getString(cancelId), listener);
+	}
+	public DialogBuilder addNegativeButton(String cancel) {
+		return addNegativeButton(cancel, null);
+	}	
+	public DialogBuilder addNegativeButton(String cancel, DialogInterface.OnClickListener listener) {
+		builder.setNegativeButton(cancel, listener);
+		return this;
+	}
+	
+	
+	
+	public DialogBuilder addPositiveButton(int cancelId) {
+		return addPositiveButton(context.getString(cancelId), null);
+	}
+	public DialogBuilder addPositiveButton(int cancelId, DialogInterface.OnClickListener listener) {
+		return addPositiveButton(context.getString(cancelId), listener);
+	}
+	public DialogBuilder addPositiveButton(String cancel) {
+		return addPositiveButton(cancel, null);
+	}	
+	public DialogBuilder addPositiveButton(String cancel, DialogInterface.OnClickListener listener) {
+		builder.setPositiveButton(cancel, listener);
+		return this;
+	}
+	
+	
+	public Dialog show() {
+		AlertDialog dialog = builder.create();
+		dialog.show();
+		TextView message = (TextView)dialog.findViewById(android.R.id.message);
+		if(message != null) {
+			message.setMovementMethod(LinkMovementMethod.getInstance());
+		}
+		return dialog;
+	}
 
 	public static Dialog showConfirmCancel(Context context, int titleId, int messageId, int confirmId, int cancelId, DialogInterface.OnClickListener confirmListener) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle(titleId);
-		builder.setMessage(messageId);
-		builder.setNegativeButton(cancelId, null);
-		builder.setPositiveButton(confirmId, confirmListener);
-		final AlertDialog dialog = builder.create();
-		dialog.setCanceledOnTouchOutside(true);
-		dialog.show();
-		return dialog;
+		return DialogBuilder.create(context, titleId, messageId)
+				.addPositiveButton(confirmId, confirmListener)
+				.addNegativeButton(cancelId)
+				.setCancelable(true)
+				.show();
 	}
 
 	public static Dialog showConfirm(Context context, int titleId, int messageId, int confirmId, DialogInterface.OnClickListener confirmListener) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle(titleId);
-		builder.setMessage(messageId);
-		builder.setPositiveButton(confirmId, confirmListener);
-		final AlertDialog dialog = builder.create();
-		dialog.setCanceledOnTouchOutside(false);
-		dialog.show();
-		return dialog;
+		return DialogBuilder
+				.create(context, titleId, messageId)
+				.addPositiveButton(confirmId, confirmListener)
+				.setCancelable(false)
+				.show();
 	}
 
 	public static Dialog show(Context context, int titleId, int messageId) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle(titleId);
-		builder.setMessage(messageId);
-		final AlertDialog dialog = builder.create();
-		dialog.setCanceledOnTouchOutside(false);
-		dialog.show();
-		return dialog;
+		return show(context, context.getString(titleId), context.getString(messageId));
 	}
 	
 	public static Dialog show(Context context, String title, String message) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle(title);
-		builder.setMessage(message);
-		final AlertDialog dialog = builder.create();
-		dialog.setCanceledOnTouchOutside(false);
-		dialog.show();
-		return dialog;
+		return show(context, title, new SpannableString(message));
+	}
+	
+	public static Dialog show(Context context, String title, SpannableString message) {
+		return DialogBuilder
+				.create(context, title, message)
+				.setCancelable(false)
+				.show();
 	}
 }
