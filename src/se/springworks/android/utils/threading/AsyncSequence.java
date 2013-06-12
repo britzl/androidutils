2b1bb1f6be2eeb4f50736ed833fe2729f759dcd8
Björn Ritzl <bjorn.ritzl@gmail.com>
2013-06-13 00:40:41 +0200

    added support for multiple listeners

--- src/se/springworks/android/utils/threading/AsyncSequence.java
+++ src/se/springworks/android/utils/threading/AsyncSequence.java
@@ -3,6 +3,10 @@ package se.springworks.android.utils.threading;
 import java.util.ArrayList;
 import java.util.List;
 
+import se.springworks.android.utils.logging.Logger;
+import se.springworks.android.utils.logging.LoggerFactory;
+import android.os.AsyncTask;
+
 
 /**
  * Utility to execute a series of async tasks in sequence and trigger a callback
@@ -12,8 +16,10 @@ import java.util.List;
  */
 public class AsyncSequence {
 	
+	private static final Logger logger = LoggerFactory.getLogger(AsyncSequence.class);
+	
 	private enum State {
-		WAITING,
+		IDLE,
 		STARTED,
 		COMPLETED
 	}
@@ -42,73 +48,207 @@ public class AsyncSequence {
 		public abstract void execute(ICallback executionCallback);
 	}
 	
-	private State state = State.WAITING;
+	private static class AsyncTaskWrapper extends AsyncCall {
+		@SuppressWarnings("rawtypes")
+		private AsyncTask task;
+		private Object params[];
+		
+		@SuppressWarnings("rawtypes")
+		public AsyncTaskWrapper(AsyncTask task, Object... params) {
+			this.task = task;
+			this.params = params;
+		}
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public void execute(final ICallback executionCallback) {
+			task.execute(params);
+			try {
+				task.get();
+				executionCallback.onDone();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				executionCallback.onError(e);
+			}
+		}
+	}
+	
+	
+	public static abstract class AsyncTaskCall extends AsyncCall {
+
+		private AsyncVoidTask task;
+		
+		@Override
+		public void execute(final ICallback executionCallback) {
+			task = new AsyncVoidTask() {
+				
+				@Override
+				protected void performTask() {
+					performAsyncTask();
+				}
+				
+				@Override
+				protected void onPostExecute() {
+					executionCallback.onDone();
+				}
+			};
+			task.execute();
+		}
+		
+		public abstract void performAsyncTask();
+	}
+	
+	private State state = State.IDLE;
 	
 	private List<AsyncCall> asyncCalls = new ArrayList<AsyncCall>();
 	
-	private ICallback callback;
+	private List<ICallback> listeners = new ArrayList<ICallback>();
+	
+	private String name;
+	
+	public AsyncSequence() {
+		this("");
+	}
+	
+	public AsyncSequence(String name) {
+		this.name = name;
+	}
 	
 	/**
 	 * Add an async call. The async calls will be made in the order that they are added
 	 * @param event
 	 */
 	public void add(AsyncCall event) {
-		asyncCalls.add(event);
+		if(isIdle()) {
+			asyncCalls.add(event);
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	public void add(AsyncTask task, Object... params) {
+		if(isIdle()) {
+			asyncCalls.add(new AsyncTaskWrapper(task, params));
+		}
 	}
 	
 	/**
-	 * Stops the sequence and removes all calls
+	 * Stops the sequence and removes all calls.
+	 * All listeners will be removed (without being called)
 	 */
-	public void stop() {
+	public synchronized void reset() {
 		asyncCalls.clear();
-		state = State.COMPLETED;
+		listeners.clear();
+		state = State.IDLE;
 	}
 	
 	/**
-	 * Start the sequence of async calls
-	 * @param callback Called once the sequence has completed and every time an error has occurred
+	 * Adds a listener to be notified when the sequence is completed
+	 * @param listener
 	 */
-	public void start(final ICallback callback) {
-		this.callback = callback;
-		if(state == State.STARTED) {
+	public synchronized void addListener(ICallback listener) {
+		if(!listeners.contains(listener)) {
+			listeners.add(listener);
+		}
+	}
+	
+	/**
+	 * Removes a listener
+	 * @param listener
+	 */
+	public synchronized void removeListener(ICallback listener) {
+		listeners.remove(listener);
+	}
+	
+	/**
+	 * 
+	 * @param listener Called once the sequence has completed and every time an error has occurred
+	 */
+	public synchronized void start(ICallback listener) {
+		addListener(listener);
+		start();
+	}
+	
+	/**
+	 * Start the sequence of async calls.
+	 * If the sequence is already started or completed nothing will happen.
+	 */
+	public synchronized void start() {
+		logger.debug("start() %s", name);
+		if(isStarted()) {
+			logger.debug("start() already started. %s", name);
 			return;
 		}
-		if(state == State.COMPLETED) {
-			callback.onDone();
+		if(isCompleted()) {
+			logger.debug("start() already completed. %s", name);
+			notifySequenceCompleted();
 			return;
 		}
+		logger.debug("start() starting. %s", name);
 		state = State.STARTED;
 		executeNext();
 	}
+	
+	public boolean isIdle() {
+		return state == State.IDLE;
+	}
+	
+	public boolean isCompleted() {
+		return state == State.COMPLETED;
+	}
+	
+	public boolean isStarted() {
+		return state == State.STARTED;
+	}
 
+	
 	/**
 	 * Executes the next async call in the sequence
 	 */
-	private void executeNext() {
+	private synchronized void executeNext() {
+//		logger.debug("executeNext() %s", name);
 		if(asyncCalls.isEmpty()) {
+//			logger.debug("executeNext() completed %s", name);
 			state = State.COMPLETED;
-			callback.onDone();
+			notifySequenceCompleted();
 			return;
 		}
 		
 		final AsyncCall event = asyncCalls.remove(0);
+//		logger.debug("executeNext() executing %s for %s", event, name);
 		event.execute(new ICallback() {
 			
 			@Override
 			public void onError(Throwable t) {
+//				logger.debug("executeNext() onError() %s", name);
 				if(event.ignoreErrors) {
 					executeNext();
 				}
 				else {
-					callback.onError(t);
+					notifySequenceError(t);
 					executeNext();
 				}
 			}
 			
 			@Override
 			public void onDone() {
+//				logger.debug("executeNext() onDone() %s", name);
 				executeNext();
 			}
 		});
+	}
+	
+	private void notifySequenceCompleted() {
+		logger.debug("notifySequenceCompleted() %s", name);
+		for(ICallback callback : listeners) {
+			callback.onDone();
+		}
+		listeners.clear();
+	}
+	
+	private void notifySequenceError(Throwable t) {
+		for(ICallback callback : listeners) {
+			callback.onError(t);
+		}
 	}
 }
