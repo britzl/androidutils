6e13bd44f3b89aefb89206ba0f3f84a858ee81ec
Björn Ritzl <bjorn.ritzl@gmail.com>
2013-06-17 10:37:38 +0200

    utility class to get app vcersion, name etc

---
+++ src/se/springworks/android/utils/application/ApplicationUtils.java
@@ -0,0 +1,49 @@
+package se.springworks.android.utils.application;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class ApplicationUtils {
+
+	private static PackageInfo getPackageInfo(Context context) {
+		PackageInfo info = null;
+		try {
+			info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+		}
+		catch (NameNotFoundException e) {
+		}
+		return info;
+	}
+	
+	public static String getVersionName(Context context) {
+		PackageInfo info = getPackageInfo(context);
+		if(info == null) {
+			return null;
+		}
+		return info.versionName;
+	}
+	
+	public static int getVersionCode(Context context) {
+		PackageInfo info = getPackageInfo(context);
+		if(info == null) {
+			return -1;
+		}
+		return info.versionCode;
+	}
+	
+	
+	public static String getApplicationName(Context context) {
+		PackageInfo info = getPackageInfo(context);
+		if(info == null || info.applicationInfo == null) {
+			return null;
+		}
+		CharSequence label = context.getPackageManager().getApplicationLabel(info.applicationInfo);
+		if(label == null) {
+			return null;
+		}
+		return label.toString();
+//		return info.applicationInfo.name;
+	}
+
+}
