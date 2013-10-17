db54ba418279f576a761f1fad7d8d6f138a45760
britzl <bjorn.ritzl@gmail.com>
2013-10-18 00:39:24 +0200

    Added mthod to convert pixels into dp. Renamed some variables

--- src/se/springworks/android/utils/view/ViewLayoutUtils.java
+++ src/se/springworks/android/utils/view/ViewLayoutUtils.java
@@ -1,5 +1,7 @@
 package se.springworks.android.utils.view;
 
+import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.ViewGroup.MarginLayoutParams;
@@ -16,17 +18,30 @@ public class ViewLayoutUtils {
 	 * @param bottom
 	 */
 	public static void setMargins(View v, int left, int top, int right, int bottom) {
-		LayoutParams lp = v.getLayoutParams();
-		if(lp == null) {
+		LayoutParams params = v.getLayoutParams();
+		if(params == null) {
 			MarginLayoutParams mlp = new MarginLayoutParams(MarginLayoutParams.WRAP_CONTENT, MarginLayoutParams.WRAP_CONTENT);
 			mlp.setMargins(left, top, right, bottom);
 			v.setLayoutParams(mlp);
 		}
-		else if(lp instanceof MarginLayoutParams) {
-			MarginLayoutParams mlp = (MarginLayoutParams)lp;
-			mlp.setMargins(left, top, right, bottom);
-			v.setLayoutParams(mlp);
+		else if(params instanceof MarginLayoutParams) {
+			MarginLayoutParams lp = (MarginLayoutParams)params;
+			lp.setMargins(left, top, right, bottom);
+			v.setLayoutParams(lp);
 		}
+	}
+	
+	public static void setMarginsDp(View v, int leftDp, int topDp, int rightDp, int bottomDp) {
+		Context ctx = v.getContext();
+		if(ctx == null) {
+			return;
+		}
+		DisplayMetrics dm = ctx.getResources().getDisplayMetrics();
+		setMargins(v, dpToPixels(dm, leftDp), dpToPixels(dm, topDp), dpToPixels(dm, rightDp), dpToPixels(dm, bottomDp));
+	}
+	
+	private static int dpToPixels(DisplayMetrics metrics, int valueInDp) {
+		return (int)(metrics.density * valueInDp + 0.5f);
 	}
 	
 	
