dbbc8f2634d040d8c7bbaed56ce225e0bf1548ca
britzl <bjorn.ritzl@gmail.com>
2013-11-19 20:13:32 +0100

    Moved Countly tracking to it's own separate library

---
+++ androidutils-countly-lib/README.md
@@ -0,0 +1,4 @@
+androidutils-countly-lib
+========================
+
+Wrapper for Countly mobile analytics (http://count.ly/). Requires the Countly GitHub project (https://github.com/Countly/countly-sdk-android).
--- androidutils-countly-lib/src/se/springworks/android/utils/analytics/CountlyTracker.java
+++ androidutils-countly-lib/src/se/springworks/android/utils/analytics/CountlyTracker.java
@@ -5,12 +5,38 @@ import se.springworks.android.utils.resource.ParameterLoader;
 import android.app.Activity;
 import android.content.Context;
 
+/**
+ * Wrapper for Countly analytics tracking
+ * You need to provide the wrapper with your Countly app key and you can optinally also define an alternative
+ * Countly server to send analytics data to. This wrapper can be configured in two ways:
+ * 
+ * 1. Define app key and server configuration as two string resources named according to the {@link CountlyTracker#KEY_SERVER} and {@link CountlyTracker#KEY_APPKEY}
+ * 2. Pass app key and optionally also server to the constructor
+ * @author bjornritzl
+ *
+ */
 public class CountlyTracker implements IAnalyticsTracker {
 	
 	private static final String KEY_SERVER = "countly_server";
 	private static final String KEY_APPKEY = "countly_appkey";
 	
+	private String appKey;
+	private String server = "http://cloud.count.ly";
+	
 	private boolean initialized = false;
+	
+	public CountlyTracker() {
+		// empty constructor for dependency injection
+	}
+	
+	public CountlyTracker(String appKey) {
+		this.appKey = appKey;
+	}
+	
+	public CountlyTracker(String appKey, String server) {
+		this.appKey = appKey;
+		this.server = server;
+	}
 
 	@Override
 	public void trackActivityStart(Activity activity) {
@@ -33,12 +59,16 @@ public class CountlyTracker implements IAnalyticsTracker {
 			return;
 		}
 		ParameterLoader loader = new ParameterLoader(context);
-		String url = "http://cloud.count.ly";
+		
 		if(loader.hasString(KEY_SERVER)) {
-			url = loader.getString(KEY_SERVER);
+			server = loader.getString(KEY_SERVER);
 		}
-		String appKey = loader.getString(KEY_APPKEY);
-        Countly.sharedInstance().init(context, url, appKey);
+		
+		if(appKey == null) {
+			appKey = loader.getString(KEY_APPKEY);
+		}
+		
+        Countly.sharedInstance().init(context, server, appKey);
 		initialized = true;
 	}
 
