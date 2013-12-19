aed4532f7a4480d0820316c7ae792d6e7b5236d7
britzl <bjorn.ritzl@gmail.com>
2013-12-19 10:09:56 +0100

    Added optional caching to the image loader

--- androidutils-lib/src/se/springworks/android/utils/image/AsyncImageLoader.java
+++ androidutils-lib/src/se/springworks/android/utils/image/AsyncImageLoader.java
@@ -2,10 +2,14 @@ package se.springworks.android.utils.image;
 
 import java.io.InputStream;
 
+import se.springworks.android.utils.cache.ICache;
+import se.springworks.android.utils.cache.ICache.CacheException;
+
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 
 public class AsyncImageLoader {
 
@@ -14,33 +18,55 @@ public class AsyncImageLoader {
 		public void onError(String error);
 	}
 	
-	@Inject
 	private IImageLoader loader;
 	
+	
+	@Inject(optional=true)
+	@Named("asyncimageloader")
+	private ICache<BitmapSerializer> cache;
+	
+	@Inject
+	public AsyncImageLoader(IImageLoader loader) {
+		this.loader = loader;
+	}
+	
 	/**
-	 * Loads a bitmap form a URL
+	 * Loads a bitmap from a URL. If a cache is set it will be checked before
+	 * loading 
 	 * @param url
 	 * @param handler
 	 */
-	public void load(String url, final OnImageLoadHandler handler) {
-		AsyncTask<String, Void, Bitmap> task = new AsyncTask<String, Void, Bitmap>() {
-
-			@Override
-			protected Bitmap doInBackground(String... params) {
-				String url = params[0];
-				return loader.getAsBitmap(url);
-			}
-			
-			@Override
-			protected void onPostExecute(Bitmap result) {
-				if(result == null) {
-					handler.onError("Unable to load bitmap");
-					return;
+	public void load(final String url, final OnImageLoadHandler handler) {
+		if(cache != null && cache.contains(url)) {
+			handler.onImage(cache.get(url).getBitmap());
+		}
+		else {
+			AsyncTask<String, Void, Bitmap> task = new AsyncTask<String, Void, Bitmap>() {
+	
+				@Override
+				protected Bitmap doInBackground(String... params) {
+					String url = params[0];
+					return loader.getAsBitmap(url);
 				}
-				handler.onImage(result);
-			}
-		};
-		task.execute(url);
+				
+				@Override
+				protected void onPostExecute(Bitmap result) {
+					if(result == null) {
+						handler.onError("Unable to load bitmap");
+						return;
+					}
+					try {
+						if(cache != null) {
+							cache.cache(url, new BitmapSerializer(result));
+						}
+					} catch (CacheException e) {
+						e.printStackTrace();
+					}
+					handler.onImage(result);
+				}
+			};
+			task.execute(url);
+		}
 	}
 	
 	public void load(InputStream in, final OnImageLoadHandler handler) {
