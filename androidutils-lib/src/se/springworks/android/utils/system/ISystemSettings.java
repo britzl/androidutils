23ff96f39c5230e45635fdd346a4812e380fee17
britzl <bjorn.ritzl@gmail.com>
2014-02-23 08:09:52 +0100

    Added checks for roaming and wifi state

--- androidutils-lib/src/se/springworks/android/utils/system/ISystemSettings.java
+++ androidutils-lib/src/se/springworks/android/utils/system/ISystemSettings.java
@@ -2,7 +2,33 @@ package se.springworks.android.utils.system;
 
 public interface ISystemSettings {
 
+	/**
+	 * Check if device is connected to a network
+	 * @return
+	 */
 	boolean isNetworkAvailable();
 	
+	/**
+	 * Check if airplane mode is enabled
+	 * @return
+	 */
 	boolean isAirplaneModeOn();
+	
+	/**
+	 * Check if data roaming is enabled
+	 * @return
+	 */
+	boolean isDataRoamingEnabled();
+	
+	/**
+	 * Check if the device is currently roaming
+	 * @return
+	 */
+	boolean isRoaming();
+	
+	/**
+	 * Check if device is connected to wifi
+	 * @return
+	 */
+	boolean isWifiConnected();
 }
--- androidutils-lib/src/se/springworks/android/utils/system/SystemSettings.java
+++ androidutils-lib/src/se/springworks/android/utils/system/SystemSettings.java
@@ -3,7 +3,11 @@ package se.springworks.android.utils.system;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 
 import com.google.inject.Inject;
 
@@ -21,9 +25,46 @@ public class SystemSettings implements ISystemSettings {
 	@Override
 	public boolean isNetworkAvailable() {
 		ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if(connectivityManager == null) {
+			return false;
+		}
 	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
 	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
 	
+	// http://stackoverflow.com/questions/12466878/how-to-check-programmatically-if-data-roaming-is-enabled-disabled
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isDataRoamingEnabled() {
+		try {
+			return Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.DATA_ROAMING) == 1;
+		} catch (SettingNotFoundException e) {
+			return false;
+		}
+	}
+	
+	@Override
+	public boolean isRoaming() {
+		ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if(connectivityManager == null) {
+			return false;
+		}
+	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+	    return activeNetworkInfo != null && activeNetworkInfo.isRoaming();
+	}
+	
+	
+	@Override
+	public boolean isWifiConnected() {
+		WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+		if(wifi == null) {
+			return false;
+		}
+		SupplicantState state = wifi.getConnectionInfo().getSupplicantState();
+		if(state == null) {
+			return false;
+		}
+		return WifiInfo.getDetailedStateOf(state) == NetworkInfo.DetailedState.CONNECTED;
+	}
 	
 }
